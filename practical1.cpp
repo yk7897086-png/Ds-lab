649fa17359547c09f659878c967e9225f08e44ee
yk7897086-png <yk7897086@gmail.com>
2025-12-30 13:26:01 +0530

    Create practical1.cpp

---
+++ practical1.cpp
@@ -0,0 +1,49 @@
+// #1 sum program
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    cout << sum;
+    return 0;
+}
+
+
+
+
+
+// for average case
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter any number";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        cout << i;
+    }
+    return 0;
+}
+
+
+
+
+// for best case
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter any no:";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            cout << i << j;
+        }
+    }
+}
