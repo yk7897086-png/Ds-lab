c0d963249b4153653db7c7274c55ca8b8fb9cd97
yk7897086-png <yk7897086@gmail.com>
2025-12-30 13:34:43 +0530

    Update practical1.cpp

--- practical1.cpp
+++ practical1.cpp
@@ -47,3 +47,40 @@ int main() {
         }
     }
 }
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int linearSearch(int a[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int key;
+    cin >> key;
+
+    int result = linearSearch(a, n, key);
+
+    cout << result;
+    return 0;
+}
+
+
+
+
